077e82e60f29841bfd1392c6b2c0f7d8345061ca
Abhineet-1502 <abhineetkr2007@gmail.com>
2025-07-11 22:34:27 +0530

    done !!!!!

---
+++ hyphen.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data={
+    'Name':['Abhineet','Manashi','Aliester','mitchell'],
+    'Age':[20,21,23,40],
+    'City':['New York','Las Vegas','Los Angelas','Amsterdam']
+}
+df=pd.DataFrame(data)
+print(df)
+csv_file='Data.csv'
+df.to_csv(csv_file, index='False')
